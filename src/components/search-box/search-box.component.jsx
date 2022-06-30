f99508a48ff512172e7e636e228f8d277490fd23
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-06-30 18:46:02 +0530

    Converted searchbox

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from "react";
 import "./search-box.styles.css";
 
-class SearchBox extends Component {
-	render() {
-		return (
-			<input
-				className={this.props.className}
-				type="search"
-				placeholder={this.props.placeholder}
-				onChange={this.props.handleChange}
-				value={this.props.value}
-			/>
-		);
-	}
+function SearchBox({ className, placeholder, handleChange, value }) {
+	return (
+		<input
+			className={className}
+			type="search"
+			placeholder={placeholder}
+			onChange={handleChange}
+			value={value}
+		/>
+	);
 }
 
 export default SearchBox;
