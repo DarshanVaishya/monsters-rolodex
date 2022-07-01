9ade49271501717c0aedc4dfc86ba75f1cf81eb5
Darshan Vaishya <DarshanVaishya3@gmail.com>
2022-07-01 17:18:07 +0530

    Misc: Cleanup commented code

--- src/App.js
+++ src/App.js
@@ -40,43 +40,4 @@ function App() {
 	);
 }
 
-/*
-class App extends Component {
-	state = {
-		monsters: [],
-		input: "",
-	};
-
-	async componentDidMount() {
-		const resp = await fetch("https://jsonplaceholder.typicode.com/users");
-		const data = await resp.json();
-		this.setState({ monsters: data });
-	}
-
-	onSearchChange = (e) => {
-		this.setState(() => ({ input: e.target.value.toLowerCase() }));
-	};
-
-	render() {
-		const { input, monsters } = this.state;
-
-		let filteredMonsters = input
-			? monsters.filter((monster) => monster.name.toLowerCase().includes(input))
-			: monsters;
-
-		return (
-			<div className="App">
-				<h1 className="app-title">Monster's rolodex</h1>
-
-				<SearchBox
-					handleChange={this.onSearchChange}
-					placeholder="Search monsters"
-					className="search-box"
-				/>
-				<CardList monsters={filteredMonsters} />
-			</div>
-		);
-	}
-}
-*/
 export default App;
